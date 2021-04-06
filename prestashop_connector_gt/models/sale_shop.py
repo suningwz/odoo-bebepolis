c43037e2d6b2ba64a1bb794e5668356458bd250d
Gabriel <teloconf@gmail.com>
2021-04-07 00:41:33 +0200

    [FIX]

--- prestashop_connector_gt/models/sale_shop.py
+++ prestashop_connector_gt/models/sale_shop.py
@@ -1382,6 +1382,7 @@ class SaleShop(models.Model):
                 product = prestashop.get('products', options={
                     'filter[id]': "[{},100000]".format(str(last_id if last_id else "0")),
                 })
+            logger.info(product)
             if product.get('products') and product.get('products').get('product'):
                 for attrs in product.get('products').get('product'):
                     data = self.get_value_data(attrs.get('attrs').get('id'))
