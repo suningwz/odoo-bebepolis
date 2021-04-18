d706f1593d28e988902214f6757a901f6600e592
Gabriel <teloconf@gmail.com>
2021-04-19 00:33:02 +0200

    [ADD] VIews

--- bebepolis_core/__manifest__.py
+++ bebepolis_core/__manifest__.py
@@ -23,9 +23,9 @@
         'prestashop_connector_gt'
     ],
     "data": [
-        #'views/pos_assets_common.xml',
-        #'views/pos_session.xml',
-        #'reports/report_cashcount.xml',
+        'views/pos_assets_common.xml',
+        'views/pos_session.xml',
+        'reports/report_cashcount.xml',
     ],
     'qweb': [
         'static/src/xml/Screens/ReceiptScreen/OrderReceipt.xml',
